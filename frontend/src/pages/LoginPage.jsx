66d219783dc31a2f98f76bb673b0ec3088e5248b
4ex0ff <225268746+4ex0ff@users.noreply.github.com>
2026-04-22 08:32:44 +0300

    Update 3 1)Поправил стили, теперь у стилей есть префиксы 2)Сделал нормальное отображение на мобилках

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,41 +6,46 @@ function LoginPage() {
     const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
 
+    const handleLogin = () => {
+        console.log('Попытка входа');
+        alert('Демо-режим: вход выполнен (заглушка)');
+        // Здесь позже будет запрос к бэкенду
+    };
 
     return (
-        <div className='page'>
-            <div className='frame'>
+        <div className="page login-page">
+            <div className="frame login-frame">
                 {/* --- Заголовок --- */}
-                <div className='title-group'>
-                    <div className='title-label'>Авторизация</div>
-                    <div className='title-line'></div>
+                <div className="login-title-group">
+                    <div className="login-title-label">Авторизация</div>
+                    <div className="login-title-line"></div>
                 </div>
 
                 {/* --- Почта --- */}
-                <div className='input-group'>
-                    <div className='input-label'>Email</div>
-                    <div className='input-wrapper'>
+                <div className="login-input-group">
+                    <div className="login-input-label">Email</div>
+                    <div className="login-input-wrapper">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M22 7L13.009 12.727C12.7039 12.9042 12.3573 12.9976 12.0045 12.9976C11.6517 12.9976 11.3051 12.9042 11 12.727L2 7" stroke="#222222" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M20 4H4C2.89543 4 2 4.89543 2 6V18C2 19.1046 2.89543 20 4 20H20C21.1046 20 22 19.1046 22 18V6C22 4.89543 21.1046 4 20 4Z" stroke="#222222" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        <input className='input-field' name='email' type='email' placeholder='Введите ваш email' />
+                        <input className="login-input-field" name='email' type='email' placeholder='Введите ваш email' />
                     </div>
-                    <div className='input-line'></div>
+                    <div className="login-input-line"></div>
                 </div>
 
                 {/* --- Пароль --- */}
-                <div className='input-group'>
-                    <div className='input-label'>Пароль</div>
-                    <div className='password-wrapper'>
-                        <div className='input-wrapper'>
+                <div className="login-input-group">
+                    <div className="login-input-label">Пароль</div>
+                    <div className="login-password-wrapper">
+                        <div className="login-input-wrapper">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 11H5C3.89543 11 3 11.8954 3 13V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V13C21 11.8954 20.1046 11 19 11Z" stroke="#222222" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M7 11V7C7 5.67392 7.52678 4.40215 8.46447 3.46447C9.40215 2.52678 10.6739 2 12 2C13.3261 2 14.5979 2.52678 15.5355 3.46447C16.4732 4.40215 17 5.67392 17 7V11" stroke="#222222" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
-                            <input className='input-field' name='password' type={showPassword ? 'text' : 'password'} placeholder='Введите ваш пароль' />
+                            <input className="login-input-field" name='password' type={showPassword ? 'text' : 'password'} placeholder='Введите ваш пароль' />
                         </div>
-                        <button className='password-toggle' type='button' onClick={() => setShowPassword(!showPassword)}>
+                        <button className="login-password-toggle" type='button' onClick={() => setShowPassword(!showPassword)}>
                             {showPassword ?
                             (<svg name='closedEyeIcon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.733 5.07599C13.0624 4.7984 15.4186 5.29081 17.4419 6.47804C19.4651 7.66527 21.0442 9.48207 21.938 11.651C22.0213 11.8755 22.0213 12.1225 21.938 12.347C21.5699 13.2377 21.0843 14.0751 20.494 14.837M14.084 14.158C13.5182 14.7045 12.7604 15.0068 11.9738 15C11.1872 14.9932 10.4348 14.6777 9.87853 14.1214C9.3223 13.5652 9.0068 12.8128 8.99996 12.0262C8.99313 11.2396 9.29551 10.4818 9.84198 9.91599" stroke="#222222" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -54,18 +59,18 @@ function LoginPage() {
                             }
                         </button>
                     </div>
-                    <div className='input-line'></div>
+                    <div className="login-input-line"></div>
                 </div>
 
                 {/*  --- Кнопка "Войти" ---   */}
-                <button className='button-primary' type='button'>
+                <button className="button-primary login-button-primary" type='button' onClick={handleLogin}>
                     Войти
                 </button>
 
                 {/*  --- Текст о регистрации ---   */}
-                <div className='register-group'>
-                    <div className='register-text'>Если у вас нет аккаунта</div>
-                    <button className='register-link' onClick={() => navigate('/register')}>
+                <div className="login-register-group">
+                    <div className="login-register-text">Если у вас нет аккаунта</div>
+                    <button className="login-register-link" onClick={() => navigate('/register')}>
                         Вы можете зарегистрироваться здесь!
                     </button>
                 </div>
