f9ff5782cb6f5b033c45c79203bb9d94c401e7d9
4ex0ff <225268746+4ex0ff@users.noreply.github.com>
2026-05-20 21:31:22 +0300

    Update 1.4 Исправлены поля в тесте auth.spec.js

--- frontend/e2e/auth.spec.js
+++ frontend/e2e/auth.spec.js
@@ -8,15 +8,15 @@ test("user can register, login and open dashboard", async ({ page }) => {
   await page.locator('input[name="name"]').fill("Учебный пользователь");
   await page.locator('input[name="email"]').fill(uniqueEmail);
   await page.locator('input[name="password"]').fill(password);
-  await page.locator('input[name="passwordConfirmation"]').fill(password);
-  await page.locator(".register-button-primary").click();
+  await page.locator('input[name="confirmPassword"]').fill(password);
+  await page.locator(".button-primary").click();
 
   await expect(page).toHaveURL(/\/login$/);
   await expect(page.locator(".form-success")).toBeVisible();
 
   await page.locator('input[name="email"]').fill(uniqueEmail);
   await page.locator('input[name="password"]').fill(password);
-  await page.locator(".login-button-primary").click();
+  await page.locator(".button-primary").click();
 
   await expect(page).toHaveURL(/\/dashboard$/);
   await expect(page.locator(".dashboard-title-label")).toHaveText("Сервис опросов");
